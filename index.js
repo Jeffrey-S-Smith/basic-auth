5b31ee1e2ea68ca030984257da241f85a5add7a4
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-09-26 22:17:40 -0700

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const { app, sequelize } = require('./src/server.js');
+
+sequelize.sync()
+  .then(() => {
+    app.listen(3000, () => console.log('server up'));
+  }).catch(e => {
+    console.error('Could not start server', e.message);
+  });
