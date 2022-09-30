e426438e0e5ec6853d7597bb7edc56d5997393be
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-09-30 15:51:32 -0700

    update

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -3,9 +3,18 @@ const { Sequelize, DataTypes } = require('sequelize');
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 const usersSchema = require('./users-model');
 
-let sequelize = new Sequelize(DATABASE_URL);
-
 const usersModel = usersSchema(sequelize, DataTypes);
+
+let herokuOptions = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+}
+
+let sequelize = new Sequelize(DATABASE_URL, herokuOptions);
 
 module.exports = {
   db: sequelize,
